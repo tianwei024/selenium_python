b6532cb0a964990ccc0b0dcaee99e5d43fdca4ce
tianwei <tianwei024@gmail.com>
2014-01-23 22:06:57 +0800

    second commit

--- 1.py
+++ 1.py
@@ -1,3 +1,7 @@
 #coding:utf-8
-def high_light():
-	pass
+'''
+selenium help function
+'''
+#高亮显示方便定位元素
+def high_light(dr,ele):
+	dr.execute_script("arguments[0].style.border = '2px solid red'",ele)
